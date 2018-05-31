862c54b5982d5ba784380e619c2c40347fc68a01
yyfishball <260861880@qq.com>
2018-05-31 17:52:05 +0800

    myOrder

--- src/utils/ImageUtils.js
+++ src/utils/ImageUtils.js
@@ -30,7 +30,34 @@ function imageUtil(e) {
   // console.log('缩放后的高: ' + imageSize.imageHeight)  
   return imageSize;  
 }  
+function imageUtil2(e,width,height){
+  var imageSize = {};  
+  var originalWidth = e.detail.width;//图片原始宽  
+  var originalHeight = e.detail.height;//图片原始高  
+  var originalScale = originalHeight/originalWidth;//图片高宽比  
+  // console.log('originalWidth: ' + originalWidth)  
+  // console.log('originalHeight: ' + originalHeight)  
+  //获取屏幕宽高  
+  var windowWidth = width;  
+  var windowHeight = height;  
+  var windowscale = windowHeight/windowWidth;//屏幕高宽比  
+  if(originalScale < windowscale){//图片高宽比小于屏幕高宽比  
+    //图片缩放后的宽为屏幕宽  
+     imageSize.imageWidth = windowWidth;  
+     imageSize.imageHeight = (windowWidth * originalHeight) / originalWidth;  
+  }else{//图片高宽比大于屏幕高宽比  
+    //图片缩放后的高为屏幕高  
+    console.log("???????");
+     imageSize.imageHeight = windowHeight;  
+     imageSize.imageWidth = (windowHeight * originalWidth) / originalHeight;  
+  }  
+  // console.log('缩放后的宽: ' + imageSize.imageWidth)  
+  // console.log('缩放后的高: ' + imageSize.imageHeight)  
+  return imageSize;  
+
+}
   
 module.exports = {  
-  imageUtil: imageUtil  
+  imageUtil: imageUtil,  
+  imageUtil2:imageUtil2
 }
